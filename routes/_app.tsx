7f7d574614c78e87122831fa1529d1834fb6a310
Justin Su <injustsu@gmail.com>
2023-12-14 18:35:43 +0800

    Remove template files

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,15 +1,15 @@
-import { AppProps } from '$fresh/server.ts'
+import { PageProps } from '$fresh/server.ts'
 
-export default function App({ Component }: AppProps) {
+export default function App({ Component }: PageProps) {
     return (
         <html>
             <head>
-                <meta charSet='utf-8' />
+                <meta charset='utf-8' />
                 <meta
                     name='viewport'
                     content='width=device-width, initial-scale=1.0'
                 />
-                <title>fresh_template</title>
+                <title></title>
             </head>
             <body>
                 <Component />
